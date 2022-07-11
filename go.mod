f84ebf58ea95fcbce6a21046f60c6c9f3c52dd9c
taichiayin <37952056+taichiayin@users.noreply.github.com>
2022-07-11 17:04:42 +0800

    https

--- go.mod
+++ go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.2 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
+	github.com/unrolled/secure v1.11.0 // indirect
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
 	golang.org/x/net v0.0.0-20220630215102-69896b714898 // indirect
 	golang.org/x/sys v0.0.0-20220627191245-f75cf1eec38b // indirect
--- main.go
+++ main.go
@@ -5,10 +5,13 @@ import (
 	"psycho-dad/routes"
 
 	"github.com/gin-gonic/gin"
+	// "github.com/unrolled/secure"
 )
 
 func main() {
 	r := gin.Default()
+	// https
+	// r.Use(TlsHandler())
 	v1 := r.Group("/v1")
 
 	routes.AddUserRouter(v1)
@@ -24,5 +27,23 @@ func main() {
 		config.StartUp()
 	}()
 
+	// r.RunTLS(":3088", "./certs/localhost.pem", "./certs/localhost-key.pem")
 	r.Run(":3088")
 }
+
+// func TlsHandler() gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		secureMiddleware := secure.New(secure.Options{
+// 			SSLRedirect: true,
+// 			SSLHost:     "localhost:3088",
+// 		})
+// 		err := secureMiddleware.Process(c.Writer, c.Request)
+
+// 		// If there was an error, do not continue.
+// 		if err != nil {
+// 			return
+// 		}
+
+// 		c.Next()
+// 	}
+// }
